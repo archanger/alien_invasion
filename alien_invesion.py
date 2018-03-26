3655d2256b413706d930b7a36ddc0924d24e733f
Kirill <chuyanov.k@gmail.com>
2018-03-26 12:44:06 +0300

    type fix

--- alien_invesion.py
+++ alien_invesion.py
@@ -1,8 +1,8 @@
 import pygame
-
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+
 
 def run_game():
 
@@ -14,7 +14,7 @@ def run_game():
 
     ship = Ship(ai_settings, screen)
 
-    while True:    
+    while True:
         gf.check_events(ship)
         ship.update()
         gf.update_screen(ai_settings, screen, ship)
--- game_functions.py
+++ game_functions.py
@@ -1,7 +1,10 @@
 import sys
+from ship import Ship
+from settings import Settings
 import pygame
 
-def check_events(ship):
+
+def check_events(ship: Ship):
     pygame.init()
     # Watch for keyboard and mouse events.
     for event in pygame.event.get():
@@ -12,11 +15,13 @@ def check_events(ship):
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
+
 def check_keydown_events(event, ship):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+
 
 def check_keyup_events(event, ship):
     if event.key == pygame.K_RIGHT:
@@ -24,7 +29,8 @@ def check_keyup_events(event, ship):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
-def update_screen(ai_settings, screen, ship):
+
+def update_screen(ai_settings: Settings, screen, ship):
     screen.fill(ai_settings.bg_color)
     ship.blitme()
     # Make the most recently drawn screen visible.
